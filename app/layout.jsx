8fea335229b236110488ec14930367feef840498
Jaelen Wright <jaelen.wright@ontariotechu.net>
2024-07-29 20:15:56 -0400

    Main features finished

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 import "@/assets/styles/globals.css";
+import "photoswipe/dist/photoswipe.css";
+
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import AuthProvider from "@/components/AuthProvider";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { GlobalProvider } from "@/context/GlobalContext";
+
 export const metadata = {
   title: "Property Pulse | Find the Perfect Rental",
   description: "Find your dream rental property",
@@ -8,11 +18,18 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
   return (
-    <html lang="en">
-      <body>
-        <div>{children}</div>
-      </body>
-    </html>
+    <GlobalProvider>
+      <AuthProvider>
+        <html lang="en">
+          <body>
+            <Navbar />
+            <main>{children}</main>
+            <Footer />
+            <ToastContainer />
+          </body>
+        </html>
+      </AuthProvider>
+    </GlobalProvider>
   );
 };
 
---
+++ utils/request.js
@@ -0,0 +1,42 @@
+const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
+
+async function fetchProperties({ showFeatured = false } = {}) {
+  try {
+    if (!apiDomain) {
+      return [];
+    }
+    const res = await fetch(
+      `${apiDomain}/properties${showFeatured ? "/featured" : ""}`,
+      { cache: "no-store" }
+    );
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+}
+
+async function fetchProperty(id) {
+  try {
+    if (!apiDomain) {
+      return null;
+    }
+    const res = await fetch(`${apiDomain}/properties/${id}`);
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+export { fetchProperties, fetchProperty };
